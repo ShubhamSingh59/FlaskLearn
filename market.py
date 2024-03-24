57dc7a39b26782a9538fb1792e9e35e52176fa02
ShubhamSingh59 <shubhams@iitgn.ac.in>
2024-03-25 00:42:21 +0530

    initial commit

--- market.py
+++ market.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, render_template_string
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
 @app.route('/')
-def hello_world():
-    return "Hello World"
+@app.route('/home') 
+def home_page():
+    return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>This is the about page of {username}</h1>'
+@app.route('/market')
+def market_page():
+    item = [
+        {'id':1, 'name':'Shubham', 'price':500},
+        {'id':2, 'name':'Sumer', 'price':500},
+        {'id':3, 'name':'Karan', 'price':500}
+    ]
+    return render_template('market.html', item = item)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
